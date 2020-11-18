28bbe08b83940ce814814d150f69e93d61d1a309
Muhammad Umar Farooq <umar@orderspot.co>
2020-11-18 20:29:27 +0500

    create database key

---
+++ step_01.js
@@ -0,0 +1,26 @@
+/*
+Fauna DB secret key:
+fnAD68eK6bACAlPh4Df77oRqA1Uzs9RtFJWy5_G7
+*/
+
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+(async () => {
+    const client = new faunadb.Client({ secret: "fnAD68eK6bACAlPh4Df77oRqA1Uzs9RtFJWy5_G7" });
+
+    try {
+        const result = await client.query(
+            q.CreateKey({
+                database: q.Database("gatsbyApp"),
+                role: "server",
+            })
+        );
+        console.log("DB secret key", result.secret);
+    } catch (e) {
+        console.log("Error", e);
+    }
+}
+
+
+)()
